1066b093ba50d38f54fcc73cc00997cc1b512e80
teachet1312 <iceninen@gmail.com>
2023-12-15 12:04:48 +0300

    fixed

--- problem 6.sql
+++ problem 6.sql
@@ -1,11 +1,8 @@
 -- индекс для поиска по названию вакансии
 create index title_vacansy_ind on vacancies(title);
 
--- индекс для поиска по минимальной зп
-create index min_salary_vacansy_ind on vacancies(salary_from);
-
--- индекс для поиска по названию города
-create index city_vacansy_ind on areas(name);
+-- индекс для поиска откликов в интеравал времени (5 задание)
+create index response_time_ind on responses2(date_of_response)
 
 -- индекс для просмотра кандидатов, которые откликнулись
 create index resp_vacansy_ind on responses2(candidate_id);
--- problem5.sql
+++ problem5.sql
@@ -1,7 +1,6 @@
 select vacancies.id, title from vacancies
 inner join responses2 on vacancies.id = responses2.vacancy_id
-where date_part('day', responses2.date_of_response - vacancies.create_date) <=7
+where responses2.date_of_response - vacancies.create_date) <= interval '7 days'
 group by vacancies.id
 having count(responses2.vacancy_id) > 5
 order by count(responses2.vacancy_id) desc;
-
